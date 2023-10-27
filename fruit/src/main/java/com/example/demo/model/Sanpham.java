05280e921094030b0a13316a291293a29d3ab189
nguyensyhai <104834023+nguyensyhai@users.noreply.github.com>
2023-10-27 14:51:59 +0700

    new

--- fruit/src/main/java/com/example/demo/model/Sanpham.java
+++ fruit/src/main/java/com/example/demo/model/Sanpham.java
@@ -1,5 +1,54 @@
-package com.example.demo.model;
+package com.example.demo;
+
+import java.util.Date;
 
 public class Sanpham {
 
+
+	private String maSP;
+	private String tenSP;
+	private String loai;
+	private Float dongia;
+	
+	public Sanpham(String maSP, String tenSP, String loai, Float dongia) {
+		super();
+		this.maSP = maSP;
+		this.tenSP = tenSP;
+		this.loai = loai;
+		this.dongia = dongia;
+	}
+
+	public String getMaSP() {
+		return maSP;
+	}
+
+	public void setMaSP(String maSP) {
+		this.maSP = maSP;
+	}
+
+	public String getTenSP() {
+		return tenSP;
+	}
+
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+
+	public String getLoai() {
+		return loai;
+	}
+
+	public void setLoai(String loai) {
+		this.loai = loai;
+	}
+
+	public Float getDongia() {
+		return dongia;
+	}
+
+	public void setDongia(Float dongia) {
+		this.dongia = dongia;
+	}
+	
+	
 }
